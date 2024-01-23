1a96166b96553d54275c9dd88a1911d11f275907
Christopher Greene <archgoon@gmail.com>
2024-01-22 17:27:05 -0800

    Add in schema.
    TODO: Add tests in this project.

---
+++ dorat/schema.py
@@ -0,0 +1,19 @@
+class DoratCall:
+    def __init__(self, js):
+        self.name = js["name"]
+        self.size = js["size"]
+        self.stackOffset = js["stackOffest"]
+
+class DoratFunction:
+    def __init__(self, js):
+        self.name = js["name"]
+        self.address = int(js["address"], 16)
+        self.arguments = js["arguments"]    # TODO: wrap this somehow
+        self.exitAddresses = [int(addr, 16) for addr in js["exitAddresses"]
+        self.variables = [DoratVariable(var) for var in js["variables"]
+        self.calls = [DoratCall(call) for call in js["calls"]]
+        self.functionBytes = js["functionBytes"]
+
+class DoratProgram:
+    def __init__(self, js):
+        self.functions = [DoratFunction(f) for f in js["functions"]]
