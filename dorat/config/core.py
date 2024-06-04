1b4e8a66c5f045de7b35caeb84942426c20d98c9
Christopher Greene <archgoon@gmail.com>
2024-06-04 15:36:27 -0700

    Further update to work to add upgrade logic.

--- dorat/config/core.py
+++ dorat/config/core.py
@@ -14,7 +14,9 @@ from enum import Enum
 
 # Exceptions
 class OldVersionException(Exception):
-    pass
+    def __init__(self, msg, old_js):
+        super().__init__(msg)
+        self.old_js = old_js
 
 class ConfigFields(str, Enum):
     name = "name"
@@ -64,19 +66,20 @@ def load_config(config_file_path=CONFIG_FILE):
     with open(config_file_path) as config_file:
         js = json.load(config_file)
         if (version:=js["version"]) != CONFIG_VERSION:
-            raise OldVersionException(f"Configuration file version '{version}' is not '{CONFIG_VERSION}'")
+            raise OldVersionException(f"Configuration file version '{version}' is not '{CONFIG_VERSION}'", js)
+    return js
 
-def write_group_to_config(group, path_ghidra, path_scripts, ghidra_version, java_home, config_file_path=CONFIG_FILE):
-    config = load_config(config_file_path)
+def write_group_to_config(group, old_config, path_ghidra, path_scripts, ghidra_version, java_home, config_file_path=CONFIG_FILE):
+    new_group = {
+        ConfigFields.name: group,
+        ConfigFields.GHIDRA_INSTALL_DIR: path_ghidra,
+        ConfigFields.GHIDRA_SCRIPTS_DIR: path_scripts,
+        ConfigFields.GHIDRA_VERSION: ghidra_version,
+        ConfigFields.JAVA_HOME: java_home
+    }
+    update_group(old_config, group, new_group)
     with open(config_file_path, "w") as json_file:
-        js = json.dumps({
-            ConfigFields.name: group,
-            ConfigFields.GHIDRA_INSTALL_DIR: path_ghidra,
-            ConfigFields.GHIDRA_SCRIPTS_DIR: path_scripts,
-            ConfigFields.GHIDRA_VERSION: ghidra_version,
-            ConfigFields.JAVA_HOME: java_home
-        })
-        update_group(config, group, js)
+        json_file.write(json.dumps(old_config,indent=2))
 
 def get_directory(prompt, default):
     print(prompt, f"({default}): ", end="")
@@ -119,24 +122,34 @@ def dirwalk(start_dir, max_depth=None, cur_depth=0, exclude=[".git"],action=None
         else:
             yield path
 
+def upgrade_dorat(old_js):
+    assert old_js["version"] == "1"
+    new_js = {"version": "2"}
+    new_js["groups"] = [old_js]
+    new_js["groups"][0]["name"] = "default"
+    return new_js
+
 def configure_dorat(config_file_path=None):
     if config_file_path is None:
         config_file_path = os.path.expanduser(CONFIG_DIR + "/dorat.json")
     dirs = os.path.dirname(config_file_path)
     if not os.path.exists(dirs):
         os.makedirs(dirs)
-    cur_config = {}
+    cur_config = {"version":"2", "groups": [{}]}
     if os.path.exists(config_file_path):
         # try to load existing
-        cur_config = load_config(config_file_path)
+        try:
+            cur_config = load_config(config_file_path)
+        except OldVersionException as exception:
+            cur_config = upgrade_dorat(exception.old_js)
+    cur_config_group = cur_config["groups"][0]
     ghidra_version = get_value("Ghidra Version",
-        cur_config.get("GHIDRA_VERSION", ghidra_config.GHIDRA_VERSION))
+        cur_config_group.get("GHIDRA_VERSION", ghidra_config.GHIDRA_VERSION))
     path_ghidra = get_directory("Path to Ghidra Root Directory",
-        cur_config.get("GHIDRA_INSTALL_DIR", os.path.expanduser("~/")))
+        cur_config_group.get("GHIDRA_INSTALL_DIR", os.path.expanduser("~/")))
     path_scripts = get_directory("Path to Ghidra Scripts Directory",
-        cur_config.get("GHIDRA_SCRIPTS_DIR", os.path.expanduser("~/")))
+        cur_config_group.get("GHIDRA_SCRIPTS_DIR", os.path.expanduser("~/")))
     java_home = get_directory("Path to Java Installation",
-        cur_config.get("JAVA_HOME", guess_java_path()))
-    write_group_to_config("default", path_ghidra, path_scripts, ghidra_version, java_home, config_file_path=config_file_path)
-
+        cur_config_group.get("JAVA_HOME", guess_java_path()))
+    write_group_to_config("default", cur_config, path_ghidra, path_scripts, ghidra_version, java_home, config_file_path=config_file_path)
 
